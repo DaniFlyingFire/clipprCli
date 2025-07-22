6f7d18b175d339b542644c3744e95958e78440fc
DaniFlyingFire <dani.brand@gmx.de>
2025-07-22 19:35:06 +0200

    add abstract to token command

--- Sources/commands/token.swift
+++ Sources/commands/token.swift
@@ -4,6 +4,7 @@ struct ClipprToken: ParsableCommand {
 
     static let configuration = CommandConfiguration(
         commandName: "token",
+        abstract: "A set of commands for managing the auth tokens",
         subcommands: [ClipprTokenAdd.self, ClipprTokenSelect.self, ClipprTokenList.self, ClipprTokenRemove.self, ClipprTokenStatus.self, ClipprTokenUnselect.self, ClipprTokenUpdate.self],
     )
 }
