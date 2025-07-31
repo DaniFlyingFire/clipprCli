01330a7fd800cb78c41d38b98f3b9bac0242f924
DaniFlyingFire <dani.brand@gmx.de>
2025-07-31 21:35:22 +0200

    add upload functionality

--- Sources/client/Connection.swift
+++ Sources/client/Connection.swift
@@ -5,11 +5,7 @@ import FoundationNetworking
 
 class Connection 
 {
-    static func fetchData(from urlString: String, token: String) async throws -> String {
-        // Validate URL
-        guard let url = URL(string: urlString) else {
-            throw URLError(.badURL)
-        }
+    static func fetchData(from url: URL, token: String) async throws -> String {
         
         // Create URLRequest
         var request = URLRequest(url: url)
@@ -31,5 +27,29 @@ class Connection
         let d = Data(base64Encoded: x[0].base64Data)!
 
         return String(data: d, encoding: .utf8)!
+    }
+
+    static func uploadData(to url: URL, token: String, text: String) async throws {
+
+        let data = try JSONEncoder().encode(text)
+
+        // Create URLRequest
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type") //
+        request.addValue(token, forHTTPHeaderField: "X-API-Key")
+        request.httpBody = data
+
+        // Send request
+        let (_, response) = try await URLSession.shared.data(for: request)
+
+        // Validate response
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw URLError(.badServerResponse)
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw URLError(.badServerResponse)
+        }
     }
 }
--- Sources/commands/clip.swift
+++ Sources/commands/clip.swift
@@ -15,8 +15,28 @@ struct ClipprClip: AsyncParsableCommand {
             throw ExitCode.failure
         }
 
+        let urlString = "https://" + token.server + "/api/clipboard"
+
+        // Validate URL
+        guard let url = URL(string: urlString) else {
+            let err = "Config invalid: URL invalid: run clippr token status"
+            if let data = (err + "\n").data(using: .utf8) {
+                FileHandle.standardError.write(data)
+            }
+            throw ExitCode.failure
+        }
+
+        if isatty(STDIN_FILENO) == 0 {
+            try await push(to: url, with: token.token)
+        } else {
+            try await pull(from: url, with: token.token) 
+        }
+        
+    }
+
+    func pull(from url: URL, with token: String) async throws {
         do {
-            let x = try await Connection.fetchData(from: "https://" + token.server + "/api/clipboard", token: token.token)
+            let x = try await Connection.fetchData(from: url, token: token)
             print(x)
         } catch {
             let err = "Something went wrong :/"
@@ -25,6 +45,26 @@ struct ClipprClip: AsyncParsableCommand {
             }
             throw ExitCode.failure
         }
-        
+    }
+
+
+    func push(to url: URL, with token: String) async throws {
+        do {
+            // Read from standard input
+            let inputData = FileHandle.standardInput.readDataToEndOfFile()
+
+            // Convert to string
+            guard let inputString = String(data: inputData, encoding: .utf8) else {
+                throw ExitCode.failure
+            }
+            try await Connection.uploadData(to: url, token: token, text: inputString)
+            
+        } catch {
+            let err = "Something went wrong :/"
+            if let data = (err + "\n").data(using: .utf8) {
+                FileHandle.standardError.write(data)
+            }
+            throw ExitCode.failure
+        }
     }
 }
